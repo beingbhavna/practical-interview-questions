2b160f7c4b9b3f9cbd24b2fe1aa95c2972fab81d
Bhavna Sharma <bhavna@i2k2.com>
2025-03-21 18:39:56 +0530

    some javascript interview questions.

--- my-js-project/demo.js
+++ my-js-project/demo.js
@@ -1,5 +1,6 @@
 
 //sort an array without using sort method sort()
+// ---------------------------
 function sortGivenArray(arr, order) {
   let temp;
   for (let i = 0; i < arr.length; i++) {
@@ -18,6 +19,7 @@ console.log(sortGivenArray([6, 4, 8, 2, 10, 30, 31], "asc"));
 console.log(sortGivenArray([6, 4, 8, 2, 10, 30, 31], "dsc"));
 
 //sort an array without using sort method sort()
+// ---------------------------
 
 const least_num = (arr) => {
   let smallest = arr[0];
@@ -46,6 +48,7 @@ const sortArray = (arr) => {
 console.log(sortArray([5, 4, 3, 2, 1]));
 
 //using bubble sort
+// ---------------------------
 
 function bubbleSort(array) {
   var done = false;
@@ -67,3 +70,129 @@ function bubbleSort(array) {
 var numbers = [12, 10, 15, 11, 14, 13, 16];
 bubbleSort(numbers);
 console.log(numbers);
+
+
+//sum of two numbers
+// ---------------------------
+
+function sum() {
+  return a + b;
+}
+let a = 5;
+let b = 10;
+sum(a, b)
+//output : 15
+
+
+// is string palindrome
+// ---------------------------
+//example 1
+let str = "Hello World, How are you?";
+function isPalindrome() {
+  return str === str.split('').reverse().join('');
+}
+isPalindrome();
+
+//output : false
+
+//example 2
+let str1 = "racecar";
+function isPalindrome() {
+  return str === str.split('').reverse().join('');
+}
+isPalindrome();
+//output : true
+
+// reverse string without create function
+// ---------------------------
+
+let string = "Hello World, How are you?";
+let reversestr = str.split('').reverse().join('');
+console.log(reversestr)
+//output : ?uoy era woH ,dlroW olleH
+
+// reverse string without create function
+// ---------------------------
+let strin = "Hello World, How are you?";
+function reverseString() {
+  return str.split('').reverse().join('');
+}
+reverseString();
+//ouput : '?uoy era woH ,dlroW olleH'
+
+//find the max number in array
+// ---------------------------
+let c = [12, 23, 45, 65, 89, 21, 98]
+function maxNumber() {
+  return Math.max(...c);
+}
+maxNumber();
+//output : 98
+
+//find the max number from given numbers that are not in array
+// ---------------------------
+let d = (12, 23, 45, 65, 89, 21, 98)
+function maxNumber() {
+  return Math.max(c);
+}
+maxNumber();
+//output : 98
+
+// note i can not find minimum number when my values are not in array format
+
+//write a program that takes an array of numbers ans return a new array with only even numbers
+// ---------------------------
+let arr = [12, 23, 45, 65, 86, 21, 98]
+function arrOfEvenNumber() {
+  return c.filter(res => res % 2 === 0)
+}
+arrOfEvenNumber();
+//output : [12, 86, 98]
+
+//sum the even elements in array
+// ---------------------------
+
+let array = [12, 23, 45, 65, 86, 21, 98]
+function arrOfEvenNumber() {
+  return c.filter(res => res % 2 === 0).reduce((sum, num) => sum + num, 0)
+}
+arrOfEvenNumber();
+//output : 196
+
+//calculate a factorial of a given number
+// ---------------------------
+let number = 21;
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+factorial(number);
+//output : 51090942171709440000
+
+
+//ig given number prime or not
+
+let qa = 17;
+function primeNumber(number) {
+  if (number <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i == 0) return false;
+  }
+  return true;
+}
+primeNumber(qa);
+//output : true
+
+
+//find largest number in nested array
+let nestedArray = [12, [23, 45], [65, [101, 21]], 98];
+function findLargestElement() {
+  // Flatten the array
+  let flatArray = nestedArray.flat(Infinity);
+  return Math.max(...flatArray);
+}
+findLargestElement();
+//output : 101
--- my-js-project/sum.js
+++ my-js-project/sum.js
@@ -1,57 +1,75 @@
 let arr = [
     {
-        id:1,
-        isValid:1,
-        studentSum:20,
-        nsetedArr:[2,4]
+        id: 1,
+        isValid: 1,
+        studentSum: 20,
+        nsetedArr: [2, 4]
     },
     {
-        id:2,
-        isValid:0,
-        studentSum:10,
-        nsetedArr:[2,2]
+        id: 2,
+        isValid: 0,
+        studentSum: 10,
+        nsetedArr: [2, 2]
     },
     {
-        id:3,
-        isValid:1,
-        studentSum:25,
-        nsetedArr:[6,4]
+        id: 3,
+        isValid: 1,
+        studentSum: 25,
+        nsetedArr: [6, 4]
     },
     {
-        id:4,
-        isValid:0,
-        studentSum:20,
-        nsetedArr:[2,3]
+        id: 4,
+        isValid: 0,
+        studentSum: 20,
+        nsetedArr: [2, 3]
     },
     {
-        id:5,
-        isValid:1,
-        studentSum:45,
-        nsetedArr:[5,4]
+        id: 5,
+        isValid: 1,
+        studentSum: 45,
+        nsetedArr: [5, 4]
     }
 ]
 
-function arrySum(){
+function arrySum() {
     let sum = 0;
-    arr.forEach(res=>{
-        if(res.isValid == 1){
+    arr.forEach(res => {
+        if (res.isValid == 1) {
             sum = parseInt(sum + res.studentSum);
-        debugger
+            debugger
         }
     });
-    console.log("studentSum",sum);
+    console.log("studentSum", sum);
     alert(sum)
 }
 arrySum();
 
-function sumNestedArray(){
+
+//OR 
+
+function arrSum(arr) {
+    let sum = 0;
+    arr.forEach(res => {
+        if (res.isValid == 1) {
+            sum = sum + res.studentSum;
+        }
+    });
+    return sum;
+}
+arrSum(arr);
+//output : 90
+
+//sum of nested array
+
+function sumNestedArray() {
 
     let sum = arr
         .filter(item => item.isValid === 1) // Filter for valid items
         .flatMap(item => item.nsetedArr) // Flatten the nested arrays
         .reduce((acc, num) => acc + num, 0); // Sum all the numbers
-    
-    console.log("sumNestedArray",sum);
-    prompt("sumNestedArray",sum)
+
+    console.log("sumNestedArray", sum);
+    prompt("sumNestedArray", sum)
 }
 sumNestedArray();
+//output : 25
